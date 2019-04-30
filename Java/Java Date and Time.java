5cf0b8d15488ccccfad133836816fc00d1015ee4
TinyNguyen <ncd.ccpn@gmail.com>
2019-04-30 23:19:01 +0700

    Java/Java Date and Time.java

---
+++ Java/Java Date and Time.java
@@ -0,0 +1,45 @@
+package solution;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+		int month = Integer.parseInt(firstMultipleInput[0]);
+
+		int day = Integer.parseInt(firstMultipleInput[1]);
+
+		int year = Integer.parseInt(firstMultipleInput[2]);
+
+		String res = Result.findDay(month, day, year);
+
+		bufferedWriter.write(res);
+		bufferedWriter.newLine();
+
+		bufferedReader.close();
+		bufferedWriter.close();
+
+	}
+}
+
+class Result {
+
+	public static String findDay(int month, int day, int year) {
+
+		java.time.LocalDate ld = java.time.LocalDate.of(year, month, day);
+		java.time.DayOfWeek date = ld.getDayOfWeek();
+
+		return String.valueOf(date);
+	}
+}
