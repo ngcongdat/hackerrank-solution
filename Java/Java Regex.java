765cb48d7037e71d8d37aff225371fee1d4a80b4
TinyNguyen <ncd.ccpn@gmail.com>
2019-05-08 21:23:00 +0700

    Java Regex.java

---
+++ Java/Java Regex.java
@@ -0,0 +1,24 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches(new MyRegex().pattern));
+		}
+	}
+}
+
+class MyRegex {
+	
+	String rules = "\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5]";
+
+	public String pattern = rules + "\\." + rules + "\\." + rules + "\\." + rules;
+
+}
